539536f83e99b162cc436180c4eeaf01011f480e
hippieperm <leess0133@gmail.com>
2025-04-29 13:48:15 +0900

    Item 클래스에 copyWith 메서드 추가

--- lib/1/models/item.dart
+++ lib/1/models/item.dart
@@ -8,4 +8,12 @@ class Item {
     required this.name,
     this.isFavorite = false,
   });
+
+  Item copyWith({int? id, String? name, bool? isFavorite}) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
+  }
 }
