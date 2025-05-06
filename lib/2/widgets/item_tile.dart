97f2b9759f7b58a572e76c105b89ff070295d138
hippieperm <leess0133@gmail.com>
2025-05-06 22:14:21 +0900

    ItemTile 생성자에서 onLike 콜백 추가 및 코드 간결화

--- lib/2/widgets/item_tile.dart
+++ lib/2/widgets/item_tile.dart
@@ -5,18 +5,26 @@ import '../viewmodels/item_view_model.dart';
 class ItemTile extends StatelessWidget {
   final Item item;
   final ItemViewModel viewModel;
+  final VoidCallback onLike;
 
   const ItemTile({
-    super.key,
+    Key? key,
     required this.item,
     required this.viewModel,
-  });
+    required this.onLike,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(viewModel.displayName),
-      subtitle: Text('ID: ${item.id}'),
+      title: Text(item.name),
+      trailing: IconButton(
+        icon: Icon(
+          item.isLiked ? Icons.favorite : Icons.favorite_border,
+          color: item.isLiked ? Colors.red : null,
+        ),
+        onPressed: onLike,
+      ),
     );
   }
 }
