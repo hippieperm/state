36acb829292ba29d516d92f1bbaadf936f164c06
hippieperm <leess0133@gmail.com>
2025-05-07 15:14:09 +0900

    아이템 목록 화면: ItemListScreen 클래스에 itemProvider를 추가하여 상태 관리 기능 구현

--- lib2/pages/item_list_screen.dart
+++ lib2/pages/item_list_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../providers/item_privider.dart';
+
 class ItemListScreen extends ConsumerWidget {
+  const ItemListScreen({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(itemProvider);
     return Scaffold();
   }
 }
