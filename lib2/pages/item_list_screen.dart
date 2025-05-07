3e22046a2c15daaaab671cd80e600ceea43ccf2c
hippieperm <leess0133@gmail.com>
2025-05-07 15:37:26 +0900

    아이템 목록 화면: ItemListScreen 클래스에서 아이템 목록을 표시하도록 수정

--- lib2/pages/item_list_screen.dart
+++ lib2/pages/item_list_screen.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers/item_privider.dart';
+import '../view/item_view_model.dart';
 
 class ItemListScreen extends ConsumerWidget {
   const ItemListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.watch(itemProvider);
-    return Scaffold();
+    final item = ref.watch(itemProvider);
+    return Scaffold(
+      appBar: AppBar(title: Text('ITEM LIST')),
+      body: ListView.builder(
+        itemCount: item.length,
+        itemBuilder: (BuildContext context, int index) {
+          final viewModel = ItemViewModel(item: item[index]);
+
+          return;
+        },
+      ),
+    );
   }
 }
