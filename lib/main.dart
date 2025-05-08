81ba21c2878511d2e2adc35a7dff6e2e7682886d
hippieperm <leess0133@gmail.com>
2025-05-08 15:23:56 +0900

    아이템 관리: ItemListScreen 클래스를 제거하고 ItemListPage로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'providers/item_provider.dart';
-import 'viewmodels/item_view_model.dart';
-import 'widgets/item_tile.dart';
-import 'models/item.dart' as model_item;
+import 'package:state/pages/item_list_page.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -15,46 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ItemListScreen(),
-    );
-  }
-}
-
-class ItemListScreen extends ConsumerWidget {
-  const ItemListScreen({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final items = ref.watch(itemProvider);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Item List'),
-      ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final viewModel = ItemViewModel(items[index]);
-          return ItemTile(
-            item: items[index],
-            viewModel: viewModel,
-            onLike: () {
-              ref.read(itemProvider.notifier).toggleLike(items[index].id);
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(itemProvider.notifier).addItem(
-                model_item.Item(
-                  id: DateTime.now().toString(),
-                  name: 'Item ${items.length + 1}',
-                ),
-              );
-        },
-        child: Icon(Icons.add),
-      ),
+      home: ItemListPage(),
     );
   }
 }
