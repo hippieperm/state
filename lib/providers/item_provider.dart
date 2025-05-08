512dbb97f5bdc701311f38f22bec68c55ffa66ad
hippieperm <leess0133@gmail.com>
2025-05-08 15:43:40 +0900

    아이템 관리: toggleLike 메서드의 코드 정리 및 가독성 향상

--- lib/providers/item_provider.dart
+++ lib/providers/item_provider.dart
@@ -13,6 +13,13 @@ class ItemProvider extends StateNotifier<List<Item>> {
   }
 
   void toggleLike(String id) {
-    
+    state = state.map(
+      (item) {
+        if (item.id == id) {
+          return Item(id: item.id, name: item.name, isLiked: !item.isLiked);
+        }
+        return item;
+      },
+    ).toList();
   }
 }
