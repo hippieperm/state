9633b3ae22f4e5dd572a1546e855b677ec59b8ee
hippieperm <leess0133@gmail.com>
2025-05-09 12:26:21 +0900

    아이템 관리: TodoItem 모델 추가 및 ItemProvider와 TodoViewModel 재구성

--- lib/view/item_list_page.dart
+++ lib/view/item_list_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state/view_models/item_provider.dart';
 import '../models/item.dart';
-import '../providers/item_provider.dart';
 
+// ConsumerWidget : Riverpod의 위젯
 class ItemListPage extends ConsumerWidget {
   const ItemListPage({super.key});
 
+  void toggleLike() {
+    // asdasd
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final items = ref.watch(itemProvider);
+    // ref : reference의 약자 -> 전역 상태 관련 데이터가 들어있는 변수
+
+    // ref.watch : 상태를 가져오고, 상태가 변할때마다 화면을 새로고침하겠다는 뜻
+    // ref.read : 전역상태 관련 데이터를 단순하게 읽겠다는 것임
+
+    final items = ref.watch(itemProvider); // itemProvider가 제공하는 상태값을 가져온다.
+
     final cnt = ref.watch(counterProvider);
 
     return Scaffold(
@@ -25,7 +36,10 @@ class ItemListPage extends ConsumerWidget {
                 color: item.isLiked ? Colors.red : null,
               ),
               onPressed: () {
-                ref.read(itemProvider.notifier).toggleLike(item.id);
+                // 전역상태 관련 데이터를 단순하게 읽는것
+                // -> itemProvider.notifier를 단순하게 가져온다.
+                final itemViewModel = (ref.read(itemProvider.notifier));
+                itemViewModel.toggleLike(item.id);
               },
             ),
           );
