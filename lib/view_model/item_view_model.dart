db8800d154ad734aedd2f294080af8f5491e7ed2
hippieperm <leess0133@gmail.com>
2025-05-10 04:58:48 +0900

    아이템 관리: itemProvider 추가

--- lib/view_model/item_view_model.dart
+++ lib/view_model/item_view_model.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:state/models/item.dart';
 
+final itemProvider = StateNotifierProvider<ItemViewModel, List<Item>>((ref) {
+  return ItemViewModel();
+});
+
 class ItemViewModel extends StateNotifier<List<Item>> {
   ItemViewModel() : super([]);
 
