035f74e1d9ca6d1ba052c37e547b3b9bc3e0612b
hippieperm <leess0133@gmail.com>
2025-05-09 17:33:16 +0900

    아이템 관리: ItemViewModel 클래스 구현 및 상태 관리 기능 추가

--- lib/viewmodel/item_view_model.dart
+++ lib/viewmodel/item_view_model.dart
@@ -1,4 +1,29 @@
-class ItemViewModel {
-  void addItem() {}
-  void toggleLiked() {}
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state/models/item.dart';
+
+class ItemViewModel extends StateNotifier<List<Item>> {
+  ItemViewModel() : super([]);
+
+  void addItem(Item item) {
+    state = [...state, item];
+  }
+
+  void toggleLiked(String name) {
+    state = state.map(
+      (e) {
+        if (e.name == name) {
+          return Item(name: e.name, isLiked: true);
+        }
+        return e;
+      },
+    ).toList();
+  }
 }
+
+final itemProvider = StateNotifierProvider<ItemViewModel, List<Item>>((ref) {
+  return ItemViewModel();
+});
+
+final counterProvider = StateProvider<int>((ref) {
+  return 1;
+});
