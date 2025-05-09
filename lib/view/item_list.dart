7ef381d8ee0ad7b5e6c25136231e4ec3a303ad84
hippieperm <leess0133@gmail.com>
2025-05-10 05:03:21 +0900

    아이템 관리: ItemList 위젯에 아이템 추가 기능 구현

--- lib/view/item_list.dart
+++ lib/view/item_list.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state/models/item.dart';
+import 'package:state/view_model/item_view_model.dart';
 
 class ItemList extends ConsumerWidget {
   const ItemList({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final item = ref.watch(itemProvider);
+    final cnt = ref.watch(cntProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('item'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(itemProvider.notifier).addItem(Item(name: 'item'));
+          ref.read(cntProvider.notifier).state++;
+        },
         child: Icon(Icons.add),
       ),
     );
