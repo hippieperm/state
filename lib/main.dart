cc71c23236caa598b06309d5a84f37fbe6a271c2
hippieperm <leess0133@gmail.com>
2025-05-09 12:44:56 +0900

    아이템 관리: 패키지 임포트 경로를 절대 경로로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'pages/item_list_page.dart';
+import 'package:state/view/item_list_page.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
