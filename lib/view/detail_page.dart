96f160f57f16673224858d92a3fc46ef1446b262
hippieperm <leess0133@gmail.com>
2025-05-10 05:39:47 +0900

    아이템 관리: DetailPage에 아이템 표시 기능 추가 및 UI 구성

--- lib/view/detail_page.dart
+++ lib/view/detail_page.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state/view_model/item_view_model.dart';
 
 class DetailPage extends ConsumerWidget {
-  const DetailPage({super.key});
+  const DetailPage({
+    required this.items,
+    super.key,
+  });
+
+  final String items;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final item = ref.watch(itemProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('detail page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('$items'),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
