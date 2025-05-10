f4af78e3b8b5d33ef320f011c238e2becbfe448f
hippieperm <leess0133@gmail.com>
2025-05-10 22:09:34 +0900

    아이템 관리: ItemPage 클래스 생성 및 기본 구조 추가

---
+++ lib/views/item_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ItemPage extends ConsumerWidget {
+  const ItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
